236c9739a76b8d5e92cabb4afd56943b0a361aa8
stock-monitor <dev@stock-monitor.local>
2026-08-24 19:42:27 +0800

    feat: make Apple design the default frontend

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,7 +3,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import { AboutPage } from './About'
-import { isAppleDesignPath } from './appRoute'
 import { initTheme } from './theme'
 import './styles.css'
 import './dark-theme.css'
@@ -11,8 +10,7 @@ import './apple-design.css'
 
 initTheme()
 const client = new QueryClient()
-const appleDesign = isAppleDesignPath()
-if (appleDesign) document.documentElement.dataset.interface = 'apple'
-else delete document.documentElement.dataset.interface
+const appleDesign = true
+document.documentElement.dataset.interface = 'apple'
 const page = window.location.pathname.replace(/\/+$/, '') === '/about' ? <AboutPage /> : <App appleDesign={appleDesign} />
 createRoot(document.getElementById('root')!).render(<StrictMode><QueryClientProvider client={client}>{page}</QueryClientProvider></StrictMode>)
